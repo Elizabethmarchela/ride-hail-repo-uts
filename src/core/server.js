c543ea5f8ba70da26de3be8172fed678bf0807c2
Elizabeth Marchela <chelarambang25@gmail.com>
2026-04-23 20:38:01 +0700

    Update

--- src/core/server.js
+++ src/core/server.js
@@ -32,7 +32,6 @@ app.use(pinoHTTP({ logger }));
 // API routes
 app.use(`${config.api.prefix}`, routes());
 
-// app.use('/api/v1', routes());
 // Handle 404 route
 app.use((request, response, next) =>
   next(errorResponder(errorTypes.ROUTE_NOT_FOUND, 'Route not found'))
