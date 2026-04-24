14e5d92fbf0367939218caeb20cadc19c2302766
cpuddinr <fuihung02@gmail.com>
2026-04-24 13:07:21 +0700

    TRIPS FINALS

---
+++ src/api/components/trips/trips-route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const tripsController = require('./trips-controller');
+
+const route = express.Router();
+
+module.exports = (app) => {
+  app.use('/trips', route);
+
+  route.post('/pickup', tripsController.pickup);
+
+  route.put('/:tripId/assign-driver', tripsController.assignDriver);
+
+  route.get('/ongoing/:passengerId', tripsController.getOngoingTrip);
+
+  route.put('/:tripId/dropoff', tripsController.dropoff);
+};
